c91331b60dabdbfa811d5362f0b87ce05a98a0dc
alvaro.navarro <alvaro.navarro@manifiesto.biz>
2023-04-24 16:05:44 +0200

    Progress bar

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,8 @@ import Vue from 'vue';
 import { App as InertiaApp, plugin as InertiaPlugin } from '@inertiajs/inertia-vue';
 import PortalVue from 'portal-vue';
 import moment from 'moment-timezone'
+import { InertiaProgress } from '@inertiajs/progress'
+
 
 moment.locale('es');
 
@@ -12,6 +14,20 @@ Vue.mixin({ methods: { route } });
 Vue.use(InertiaPlugin);
 Vue.use(PortalVue);
 
+InertiaProgress.init({
+    // The delay after which the progress bar will
+    // appear during navigation, in milliseconds.
+    delay: 250,
+  
+    // The color of the progress bar.
+    color: '#29d',
+  
+    // Whether to include the default NProgress styles.
+    includeCSS: true,
+  
+    // Whether the NProgress spinner will be shown.
+    showSpinner: false,
+  })
 const app = document.getElementById('app');
 
 new Vue({
